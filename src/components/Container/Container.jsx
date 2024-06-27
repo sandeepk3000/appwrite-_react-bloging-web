79c8078739695cdc175bc83debb12052ddcbd7cf
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2024-06-27 13:20:21 +0530

    Update Container.jsx
    Add px in container

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Container({ children }) {
   return (
-    <div class="max-w-screen-xl mx-auto">
+    <div class="max-w-screen-xl mx-auto px-4">
       {children}
     </div>
   );
