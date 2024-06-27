6933d9db01a13f7c7afd513d35fe88864cba4ae4
sandeep kumar <148047316+sandeepk3000@users.noreply.github.com>
2024-06-27 13:31:07 +0530

    Update Footer.jsx
    Footer link remove add about

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,22 +9,10 @@ function Footer() {
             <div className="mx-auto w-full max-w-screen-3xl">
                 <div className="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-3">
                     <div>
-                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Topices</h2>
+                        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">COMPANY</h2>
                         <ul className="text-gray-500 dark:text-gray-400 font-medium">
                             <li className="mb-4">
-                                <Link to={"/articles/health"} href="#" className=" hover:underline">Health</Link>
-                            </li>
-                            <li className="mb-4">
-                                <Link to={"/articles/education"} href="#" className="hover:underline">Education</Link>
-                            </li>
-                            <li className="mb-4">
-                                <Link to={"/articles/tech"} href="#" className="hover:underline">Tech</Link>
-                            </li>
-                            <li className="mb-4">
-                                <Link to={"/articles/job"} href="#" className="hover:underline">Job</Link>
-                            </li>
-                            <li className="mb-4">
-                                <Link to={"/articles/random"} href="#" className="hover:underline">Random</Link>
+                                <Link to={""}className="hover:underline">About</Link>
                             </li>
                         </ul>
                     </div>
