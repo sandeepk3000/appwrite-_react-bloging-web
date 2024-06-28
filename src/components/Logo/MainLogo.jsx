d78098f7e96b78e2b6be08ed1c1e9fc87456e71f
sandeepk3000 <codejourney@gmail.com>
2024-06-27 23:44:45 -0700

    rk add logo in appwrite and main logo file preview

--- src/components/Logo/MainLogo.jsx
+++ src/components/Logo/MainLogo.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Serive from '../../appwrite/config'
 function MainLogo({ name = "" }) {
     return (
         < div className="inline-flex items-center space-x-2" >
             <span className="bg-orange-50 overflow-hidden rounded-full">
                 <Link to={"/"}>
-                    <img src="src\assets\IMG-20240627-WA0000.jpg" height={"50"} width={"50"} alt="" />
+                    <img src={Serive.getFilePreview("667e5b4400052f8e7cac")} height={"50"} width={"50"} alt="" />
 
                 </Link>
             </span>
